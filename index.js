1dfcdb4eb873bb27e4925e48b557c0f16e4f7ee7
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-01-28 16:35:38 -0500

    Pass unit tests for tallyUpDonations

--- index.js
+++ index.js
@@ -240,8 +240,14 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  const sumOfDonations = runners.reduce(function(accumulator, item) {
+    return accumulator + item.donation;
+  }, 0);
+
+
+
+  return sumOfDonations;
 }
 
 /////////////// CLOSURES ///////////////
