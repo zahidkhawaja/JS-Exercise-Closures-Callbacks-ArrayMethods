f7071e278428bcc410a3635a3baa8bd24cae4408
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-01-28 15:26:26 -0500

    Pass unit test for processLastItem

--- index.js
+++ index.js
@@ -66,8 +66,9 @@ function processLength(list, callback) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+function processLastItem(stringList, callback) {
+  let lastItem = stringList.length - 1;
+  return callback(stringList[lastItem]);
 }
 
 /**
