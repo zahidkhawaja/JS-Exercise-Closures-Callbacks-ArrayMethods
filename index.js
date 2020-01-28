bc42f1036c46c792b50a2a7cd6968afebcb965df
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-01-28 16:21:39 -0500

    Pass unit tests for firstNamesAllCaps

--- index.js
+++ index.js
@@ -202,8 +202,11 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+
+  const allCapsArray = runners.map(item => item.first_name.toUpperCase());
+
+  return allCapsArray;
 }
 
 /**
