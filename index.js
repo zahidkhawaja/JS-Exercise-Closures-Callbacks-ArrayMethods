2d0cd7c3b49092a4805ddd35e9d334d5d7af37de
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-01-28 16:29:42 -0500

    Pass unit tests for getRunnersByTShirtSize

--- index.js
+++ index.js
@@ -222,8 +222,12 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  const filteredRunners = runners.filter(function(item) {
+    return item.shirt_size == tShirtSize;
+  });
+  
+    return filteredRunners;
 }
 
 /**
